aa870a6e107ef568daeb0e5de9e4f026da84d77e
foulegold@gmail.com <foulegold@gmail.com>
2021-08-06 13:11:17 +0700

    Home work 3

--- resources/views/index.php
+++
@@ -1,5 +0,0 @@
-<h1>Hello, World!</h1>
-<p><a href="<?= route('news')?>">Все новости</a></p>
-<p><a href="<?= route('news.categories')?>">Категории</a></p>
-<p><a href="<?= route('news.currentCategory', ['id' => '4'])?>">Новости по категории №4</a></p>
-<p><a href="<?= route('news.show', ['id' => '12'])?>">Новость №12</a></p>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Clean Blog - Start Bootstrap Theme</title>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+</head>
+<body>
+<x-main.navigation></x-main.navigation>
+<x-main.header></x-main.header>
+
+@yield('content');
+
+<x-main.footer></x-main.footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('js/scripts.js') }}"></script>
+</body>
+</html>
